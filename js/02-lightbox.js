093ff0c43ef8f81e41fc5f8651ea232b8790af8e
Alexey-2903 <73944684+Alexey-2903@users.noreply.github.com>
2022-08-16 12:29:19 +0300

    Add files via upload

---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = createGallery(galleryItems);
+// const cardMarkup = createGallery(galleryItems);
+
+// galleryEl.insertAdjacentHTML('afterbegin', cardMarkup);
+
+function createGallery(array) {
+    return array.map(({ preview, original, description }) => {
+        return `<a class="gallery__item"  href="${original}">
+    <img class="gallery__image" src=${preview} alt=${description} />
+</a>`;
+    }).join('');
+};
+
+const simpleLightBox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+// console.log(galleryItems);
